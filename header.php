c1c0e49a36fd5d103417965b6229a3c137dabf15
girapha <contato@girapha.com.br>
2020-08-18 18:02:37 -0300

    aplicando algumas tags php do wp e inserindo a imagem padrão do tema

--- header.php
+++ header.php
@@ -3,13 +3,13 @@
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
 <!--[if gt IE 8]><!-->
-<html class="no-js">
+<html class="no-js" <?php language_attributes(); ?>>
 <!--<![endif]-->
 
 <head>
-  <meta charset="utf-8">
+  <meta charset="<?php bloginfo('charset'); ?>">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>KVA Transformadores | Transformando sua Energia</title>
+  <title><?php bloginfo('name'); ?></title>
   <meta name="description" content="">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/public/css/main.min.css">
