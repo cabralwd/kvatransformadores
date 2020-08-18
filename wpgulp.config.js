7737d7f1288aceb7ba09c610bd35c4bc5df563de
girapha <contato@girapha.com.br>
2020-08-18 16:37:46 -0300

    Imagens e ícones do projeto + configuração do WPGulp

--- wpgulp.config.js
+++ wpgulp.config.js
@@ -12,7 +12,7 @@ module.exports = {
 	// Project options.
 	projectURL: 'kvatransformadores.local/', // Local project URL of your already running WordPress site. Could be something like wpgulp.local or localhost:3000 depending upon your local WordPress setup.
 	productURL: './', // Theme/Plugin URL. Leave it like it is, since our gulpfile.js lives in the root folder.
-	browserAutoOpen: true,
+	browserAutoOpen: false,
 	injectChanges: true,
 
 	// Style options.
@@ -33,8 +33,8 @@ module.exports = {
 	jsCustomFile: 'custom', // Compiled JS custom file name. Default set to custom i.e. custom.js.
 
 	// Images options.
-	imgSRC: './assets/img/raw/**/*', // Source folder of images which should be optimized and watched. You can also specify types e.g. raw/**.{png,jpg,gif} in the glob.
-	imgDST: './assets/img/', // Destination folder of optimized images. Must be different from the imagesSRC folder.
+	imgSRC: './assets/images/**/*', // Source folder of images which should be optimized and watched. You can also specify types e.g. raw/**.{png,jpg,gif} in the glob.
+	imgDST: './public/imgs/', // Destination folder of optimized images. Must be different from the imagesSRC folder.
 
 	// Watch files paths.
 	watchStyles: './assets/scss/**/*.scss', // Path to all *.scss files inside css folder and inside them.
@@ -54,16 +54,16 @@ module.exports = {
 	// Browsers you care about for autoprefixing. Browserlist https://github.com/ai/browserslist
 	// The following list is set as per WordPress requirements. Though, Feel free to change.
 	BROWSERS_LIST: [
-		'last 2 version',
+		'last 4 version',
 		'> 1%',
 		'ie >= 11',
-		'last 1 Android versions',
-		'last 1 ChromeAndroid versions',
-		'last 2 Chrome versions',
-		'last 2 Firefox versions',
-		'last 2 Safari versions',
-		'last 2 iOS versions',
-		'last 2 Edge versions',
-		'last 2 Opera versions'
+		'last 3 Android versions',
+		'last 3 ChromeAndroid versions',
+		'last 4 Chrome versions',
+		'last 4 Firefox versions',
+		'last 4 Safari versions',
+		'last 4 iOS versions',
+		'last 4 Edge versions',
+		'last 4 Opera versions'
 	]
 };
