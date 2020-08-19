2945441ceadeae50ae5b6e192b0bf77628d4cb87
girapha <contato@girapha.com.br>
2020-08-19 17:48:14 -0300

    inserindo tag do wp para scripts

--- footer.php
+++ footer.php
@@ -1,5 +1,8 @@
-
-  <script src="<?php echo get_stylesheet_directory_uri(); ?>/public/js/vendor.min.js" async defer></script>
+<footer class="footer-dev">
+  <p>Design by <a href="https://www.girapha.com.br" target="_blank" rel="noopener noreferrer">girapha.com.br</a></p>
+</footer>
+<?php wp_footer(); ?>
+<script src="<?php echo get_stylesheet_directory_uri(); ?>/public/js/vendor.min.js" async defer></script>
 </body>
 
 </html>
